f560c8e301ee8bfb97af67d773c136ec7cb76fef
Akichan0201 <adzkiaalmaghania701@gmail.com>
2025-05-27 14:36:57 +0700

    update host

--- main.py
+++ main.py
@@ -23,6 +23,6 @@ async def get_distance():
     return distance()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
 
 
